47a539372e8f69a2610d5e1cf46c2f086ff8ac81
Cristian Gonzalez <cmgonzalez@gmail.com>
2018-02-15 02:17:18 -0300

    fix includes, they didn't work here...

--- game_engine.c
+++ game_engine.c
@@ -24,7 +24,7 @@
 #include "game_sprite.h"
 #include "game_zx.h"
 #include "macros.h"
-#include <nirvana+.h>
+#include <arch/zx/nirvana+.h>
 #include <arch/zx.h>
 #include <intrinsic.h>
 #include <stdlib.h>
